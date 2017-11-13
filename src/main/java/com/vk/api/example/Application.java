676d671e6d9a0a285f39343bdd36776ec932d82b
artsevruk <artsevruk@gmail.com>
2017-11-13 16:28:33 +0300

    fix

--- src/main/java/com/vk/api/example/Application.java
+++ src/main/java/com/vk/api/example/Application.java
@@ -1,5 +1,4 @@
 package com.vk.api.example;
-
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.exceptions.ApiException;
@@ -11,21 +10,31 @@ import org.slf4j.LoggerFactory;
 import java.io.FileNotFoundException;
 import java.util.Random;
 
+
+/**
+ *
+ */
 public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
-    private final VkApiClient apiClient;
-    private final UserActor actor;
+    private VkApiClient vk;
+    private UserActor actor;
     private final Random random = new Random();
 
-    public Application(VkApiClient apiClient, UserActor actor) {
-        this.apiClient = apiClient;
+    public Application(VkApiClient vk, UserActor actor){
+        this.vk = vk;
         this.actor = actor;
     }
 
+    /**
+     * Отправляет сообщение ползователю
+     *
+     * @param userId      - id пользователя
+     * @param textMessage - текст сообщения
+     */
     public void sendMessage(int userId, String textMessage) {
         try {
-            apiClient.messages().send(actor).message(textMessage).userId(userId).randomId(random.nextInt()).execute();
+            vk.messages().send(actor).message(textMessage).userId(userId).randomId(random.nextInt()).execute();
         } catch (ApiException e) {
             LOG.error("INVALID REQUEST", e);
         } catch (ClientException e) {
@@ -35,19 +44,13 @@ public class Application {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        //267912605
-
-
         HttpTransportClient client = new HttpTransportClient();
-        VkApiClient apiClient = new VkApiClient(client);
-
-        UserActor actor = RequestHandler.initVkApi(RequestHandler.readProperties());
-
-        Application application = new Application(apiClient,actor);
-        application.sendMessage(267912605, "Hello my friend Artem . .");
+        VkApiClient vk = new VkApiClient(client);
 
 
-
+        //267912605
+        Application application = new Application(vk, InitVkApi.initActor(InitVkApi.readProperties()));
+        application.sendMessage(3, "Hello my friend Artem . .");
 
 
     }
--- src/main/java/com/vk/api/example/InitVkApi.java
+++ src/main/java/com/vk/api/example/InitVkApi.java
@@ -1,27 +1,40 @@
 package com.vk.api.example;
-
-import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class RequestHandler {
+/**
+ * Класс для инициализации API Вконтакте
+ */
+
+public class InitVkApi {
 
     private final static String PROPERTIES_FILE = "config.properties";
 
-    public static UserActor initVkApi(Properties properties) {
+    /**
+     * Инициализируем, авторизируем пользователя
+     * @param properties
+     * @return UserActor - авторизованый пользователь, от имени которого будет использоваться API
+     */
+    public static UserActor initActor(Properties properties) {
         int userId = Integer.parseInt(properties.getProperty("userId"));
         String token = properties.getProperty("token");
         if (userId == 0 || token == null) throw new RuntimeException("Params are not set");
         return new UserActor(userId, token);
     }
 
+    /**
+     * Считываем данные из файла "config.properties"
+     * @return Properties
+     * @throws FileNotFoundException
+     */
     public static Properties readProperties() throws FileNotFoundException {
-        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
-        if (inputStream == null)
+        InputStream inputStream = InitVkApi.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
+        if (inputStream == null){
             throw new FileNotFoundException("property file '" + PROPERTIES_FILE + "' not found in the classpath");
+        }
 
         try {
             Properties properties = new Properties();
